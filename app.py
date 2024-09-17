2ef4f115fe0e7dd599caa22d860d1f076afc2084
Swooshoo <eliotlee1415@gmail.com>
2024-09-17 14:00:42 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="TFT Set 1 Dashboard", page_icon=":penguin:")
+
+# LOAD DATA ONCE
+@st.cache_resource
+def load_data():
+    path = "uber-raw-data-sep14.csv.gz"
+    if not os.path.isfile(path):
+        path = f"https://github.com/streamlit/demo-uber-nyc-pickups/raw/main/{path}"
+
+    data = pd.read_csv(
+        path,
+        nrows=100000,  # approx. 10% of data
+        names=[
+            "date/time",
+            "lat",
+            "lon",
+        ],  # specify names directly since they don't change
+        skiprows=1,  # don't read header since names specified directly
+        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
+        parse_dates=[
+            "date/time"
+        ],  # set as datetime instead of converting after the fact
+    )
+
+    return data
