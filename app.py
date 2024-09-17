5e3fdeeee749482c3cbfa4232493bddad9fb02fd
Swooshoo <eliotlee1415@gmail.com>
2024-09-17 14:18:12 -0700

    updated csv with magic resist

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import pandas as pd
 import pydeck as pdk
 import streamlit as st
 
-st.set_page_config(layout="wide", page_title="TFT Set 1 Dashboard", page_icon=":penguin:")
+st.set_page_config(layout="wide", page_title="TFT Set 3 Dashboard", page_icon=":penguin:")
 
 # LOAD DATA ONCE
 @st.cache_resource
 def load_data():
-    path = "uber-raw-data-sep14.csv.gz"
+    path = "Set3Champions.csv"
     if not os.path.isfile(path):
         path = f"https://github.com/streamlit/demo-uber-nyc-pickups/raw/main/{path}"
 
@@ -19,9 +19,15 @@ def load_data():
         path,
         nrows=100000,  # approx. 10% of data
         names=[
-            "date/time",
-            "lat",
-            "lon",
+            "unit",
+            "cost",
+            "health",
+            "armor",
+            "magic_resist"
+            "attack",
+            "attack_range",
+            "attack_speed",
+            
         ],  # specify names directly since they don't change
         skiprows=1,  # don't read header since names specified directly
         usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
