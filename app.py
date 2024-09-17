953290124f420067c01144af8db81d4d13835825
Swooshoo <eliotlee1415@gmail.com>
2024-09-17 15:10:36 -0700

    Updated Data, and updated file paths

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def load_data():
 
     data = pd.read_csv(
         path,
-        nrows=100000,  # approx. 10% of data
+        nrows=52,  # 52 units in set 3
         names=[
             "unit",
             "cost",
@@ -30,10 +30,8 @@ def load_data():
             
         ],  # specify names directly since they don't change
         skiprows=1,  # don't read header since names specified directly
-        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
-        parse_dates=[
-            "date/time"
-        ],  # set as datetime instead of converting after the fact
+        usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],  # uses all columns
+        
     )
 
     return data
