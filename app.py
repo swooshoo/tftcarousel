5091b2276fa1bb87d16f9eb142776ef4b9a229c1
Swooshoo <eliotlee1415@gmail.com>
2024-09-20 14:02:25 -0700

    added resizable dashboard above

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 
-st.set_page_config(layout="wide", page_title="TFT Set 3 Dashboard", page_icon=":penguin:")
+st.set_page_config(layout="wide", page_title="TFT Set 12 Dashboard", page_icon=":penguin:")
 
 # LOAD DATA ONCE
 @st.cache_resource
@@ -38,26 +38,6 @@ def load_data():
     data['class'] = data['class'].str.split('/')
     
     return data
-
-def create_tft_grid():
-    st.title("Teamfight Tactics Set 3 Units Grid")
-
-    # Load the data
-    df = load_data()
-
-    # Create a 52-cell grid (7x8 with some empty cells)
-    cols = st.columns(8)
-    for i in range(52):
-        if i < len(df):
-            unit = df.iloc[i]
-            with cols[i % 8]:
-                st.write(f"**{unit['unit']}**")
-                st.write(f"Cost: {unit['cost']}")
-                st.write(f"Origin: {unit['origin']}")
-                st.write(f"Class: {', '.join(unit['class'])}")
-        else:
-            with cols[i % 8]:
-                st.empty()
                 
 from streamlit_elements import elements, mui, html, dashboard
 
@@ -69,18 +49,12 @@ def create_dashboard(): #resizable, draggable dashboard
     # First, build a default layout for every element you want to include in your dashboard
         layout = [
             # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-            dashboard.Item("first_item", 0, 0, 2, 2),
-            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=True, moved=True),
-            dashboard.Item("third_item", 0, 2, 1, 1, isResizable=True),
+            dashboard.Item("first_item", 0, 0, 4, 2),
+            dashboard.Item("second_item", 4, 2, 4, 2, isDraggable=True, isResizable=True),
+            dashboard.Item("third_item", 8, 2, 4, 2, isDraggable= True, isResizable=True),
         ]
-
         # Next, create a dashboard layout using the 'with' syntax. It takes the layout
         # as first parameter, plus additional properties you can find in the GitHub links below.
-
-        with dashboard.Grid(layout):
-            mui.Paper("First item", key="first_item")
-            mui.Paper("Second item ", key="second_item")
-            mui.Paper("Third item ", key="third_item")
 
         # If you want to retrieve updated layout values as the user move or resize dashboard items,
         # you can pass a callback to the onLayoutChange event parameter.
@@ -91,10 +65,10 @@ def create_dashboard(): #resizable, draggable dashboard
             print(updated_layout)
 
         with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
-            mui.Paper("First item", key="first_item")
-            mui.Paper("Second item ", key="second_item")
-            mui.Paper("Third item ", key="third_item")
-            
+            mui.Typography("Selected Unit", key="first_item")
+            mui.Paper("Traits ", key="second_item")
+            mui.Paper("Ability", key="third_item")
+
 
 def image_select_demo():
     from streamlit_image_select import image_select
@@ -102,32 +76,38 @@ def image_select_demo():
     img = image_select(
         label="Select a unit",
         images=[
-            "briar.png",
-            "camille.jpg",
-            "diana.png",
-            "milio.webp",
-            "morgana.png",
-            "norra.png",
-            "smolder.png",
-            "xerath.png",
-            "fiora.png",
-            "gwen.png",
-            "kalista.png",
-            "karma.png",
-            "nami.png",
-            "nasus.png",
-            "olaf.png",
-            "rakan.png",
-            "ryze.png",
-            "tahmkench.png",
-            "taric.png",
-            "varus.png",
+            "images/briar.png",
+            "images/camille.jpg",
+            "images/diana.png",
+            "images/milio.webp",
+            "images/morgana.png",
+            "images/norra.png",
+            "images/smolder.png",
+            "images/xerath.png",
+            "images/fiora.png",
+            "images/gwen.png",
+            "images/kalista.png",
+            "images/karma.png",
+            "images/nami.png",
+            "images/nasus.png",
+            "images/olaf.png",
+            "images/rakan.png",
+            "images/ryze.png",
+            "images/tahmkench.png",
+            "images/taric.png",
+            "images/varus.png",
         ],
 
         captions=["Briar", "Camille","Diana","Milio","Morgana","Norra","Smolder","Xerath","Fiora","Gwen","Kalista","Karma","Nami","Nasus","Olaf","Rakan","Ryze","Tahm Kench","Taric","Varus",],
-        use_container_width=False
+        use_container_width=False,
+        return_value="index"
     )
+    st.write(str(img)[:100])
+    
+    #need to implement a while statement. default value = 0. while img is a valid number, show a card fr
+    
+
 if __name__ == "__main__":
     #create_tft_grid()
-    #create_dashboard()
+    create_dashboard()
     image_select_demo()
