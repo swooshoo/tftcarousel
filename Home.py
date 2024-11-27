ad7a654b65513fb4f2fcd1eccba9f3e4d1e39dce
swooshoo <102700240+swooshoo@users.noreply.github.com>
2024-11-27 01:24:57 -0800

    Home.py changes

--- Home.py
+++ Home.py
@@ -11,6 +11,14 @@ st.set_page_config(
 st.title("TFT Carousel")
 st.text("An introduction for anyone curious about the game!")
 
+st.markdown(
+    '''
+I noticed with their recent success with their Netflix Series "Arcane" that many new players are speculating to join Riot Games' Teamfight Tactics, an auto-chess style game with a relatively tall knowledge curve. 
+While there are various sites that provide various statistics and dashboards for experienced players, very little provide basic, UI/UX friendly knowledge that even I myself as an experienced player find hard to use.
+
+Hopefullly this site helps ease new players into trying the game before throwing them into the overwhelming data many sites throw at a user.
+    '''
+)
 
 st.markdown(
 
