15d46ce02de6aaba37c368340ecb61041fcd94ee
swooshoo <102700240+swooshoo@users.noreply.github.com>
2024-11-27 01:11:55 -0800

    updated Home.py

--- Home.py
+++ Home.py
@@ -2,13 +2,16 @@ import streamlit as st
 import pandas as pd
 
 # Displaying some content in Home.py
-st.title("TFT Carousel")
-st.text("An introduction for anyone curious about the game!")
 
 st.set_page_config(
     page_title="TFT Carousel",
     page_icon="static/tft_icon.png",
 ) 
+
+st.title("TFT Carousel")
+st.text("An introduction for anyone curious about the game!")
+
+
 st.markdown(
 
     '''
